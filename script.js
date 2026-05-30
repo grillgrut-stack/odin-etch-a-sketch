8e0a33f9776fb6149440c6e457c08debbdc6d279
grillgrut <grillgrut@gmail.com>
2026-05-30 12:46:35 -0500

    Change the script to allow for the deletion and creation of a new grid with a user input

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const container = document.querySelector("#container");
+container = document.querySelector("#container");
 
 
 
 for (let i = 0; i < 64; i++) {
     const newContainer = document.createElement("div");
+    newContainer.classList.add("outer");
     container.appendChild(newContainer);
     for (let z = 0; z < 64; z++) {
         const divs = document.createElement("div");
@@ -15,4 +16,48 @@ for (let i = 0; i < 64; i++) {
         newContainer.appendChild(divs);
     }
 }
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const wrapper = document.querySelector("#big");
+    wrapper.removeChild(container)
+
+
+    while (true) {
+        
+    let newGrid = prompt("Please Enter a new grid size # x #");
+
+    if (newGrid > 100) {
+        newGrid = prompt("Pick a number less than or equal to 100!")
+    }
+
+    }
+
+
+
+    container = document.createElement("div");
+    container.id = "container"
+    wrapper.appendChild(container);
+
+
+    for (let i = 0; i < newGrid; i++) {
+
+        const newContainer = document.createElement("div");
+        newContainer.classList.add("outer");
+        container.appendChild(newContainer);
+
+    for (let z = 0; z < newGrid; z++) {
+
+        const divs = document.createElement("div");
+        divs.textContent = "";
+        divs.classList.add("square");
+        divs.addEventListener("mouseenter", () => {
+        divs.style.backgroundColor = "blue";
+        });
+
+        newContainer.appendChild(divs);
+        }
+    }
+});
+
 
