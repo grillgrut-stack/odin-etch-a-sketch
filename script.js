42dc672930206183bcc25cb20f41f2861d0a51a0
grillgrut <grillgrut@gmail.com>
2026-05-30 11:42:05 -0500

    Change the 16 x 16 to be stacked on top of each other

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const container = document.querySelector("#container");
 
 
-
-
-const divs = document.createElement("div");
-divs.textContent = "test";
-
-
 for (let i = 0; i < 16; i++) {
+    const newContainer = document.createElement("div");
+    container.appendChild(newContainer);
     for (let z = 0; z < 16; z++) {
         const divs = document.createElement("div");
         divs.textContent = "test";
-        container.appendChild(divs);
+        newContainer.appendChild(divs);
     }
 }
