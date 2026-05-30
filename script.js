8e339ba5fe1e7321f9878aafa4da17c79d36893e
grillgrut <grillgrut@gmail.com>
2026-05-30 11:39:05 -0500

    Change the script to be 16 x 16 text

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ const container = document.querySelector("#container");
 const divs = document.createElement("div");
 divs.textContent = "test";
 
+
 for (let i = 0; i < 16; i++) {
-    const divs = document.createElement("div");
-    divs.textContent = "test";
-    container.appendChild(divs);
+    for (let z = 0; z < 16; z++) {
+        const divs = document.createElement("div");
+        divs.textContent = "test";
+        container.appendChild(divs);
+    }
 }
