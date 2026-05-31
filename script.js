60d7bf8bc721a4d3abb1a526c3b5abfd6756b19d
grillgrut <grillgrut@gmail.com>
2026-05-31 10:10:05 -0500

    Made the grid creation a funciton

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 container = document.querySelector("#container");
 
+let gridSize = 16;
 
+function createGrid (gridSize) {
+    for (let i = 0; i < gridSize; i++) {
 
-for (let i = 0; i < 64; i++) {
-    const newContainer = document.createElement("div");
-    newContainer.classList.add("outer");
-    container.appendChild(newContainer);
-    for (let z = 0; z < 64; z++) {
-        const divs = document.createElement("div");
-        divs.textContent = "";
-        divs.classList.add("square");
-        divs.addEventListener("mouseenter", () => {
-            divs.style.backgroundColor = "blue";
-        });
-        newContainer.appendChild(divs);
+        const newContainer = document.createElement("div");
+        newContainer.classList.add("outer");
+        container.appendChild(newContainer);
+
+        for (let z = 0; z < gridSize; z++) {
+
+            const divs = document.createElement("div");
+            divs.textContent = "";
+            divs.classList.add("square");
+            divs.addEventListener("mouseenter", () => {
+                divs.style.backgroundColor = "blue";
+            });
+            newContainer.appendChild(divs);
+        }
     }
 }
+
 
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
@@ -62,3 +68,4 @@ button.addEventListener("click", () => {
 });
 
 
+createGrid(gridSize);
