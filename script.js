0ff610e8a3e5ceb9190e41db2549fe4cc60e5b91
grillgrut <grillgrut@gmail.com>
2026-05-31 10:12:38 -0500

    Make the button use the createGrid function

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ function createGrid (gridSize) {
     }
 }
 
-
 const button = document.querySelector("button");
+
 button.addEventListener("click", () => {
+
     const wrapper = document.querySelector("#big");
     wrapper.removeChild(container)
 
@@ -32,39 +33,18 @@ button.addEventListener("click", () => {
 
     while (true) {
 
-    if (newGrid > 100) {
-        newGrid = prompt("Pick a number less than or equal to 100!")
-    } else {
-        break;
+        if (newGrid > 100) {
+            newGrid = prompt("Pick a number less than or equal to 100!")
+        } else {
+            break;
+        }
     }
-
-    }
-
-
 
     container = document.createElement("div");
     container.id = "container"
     wrapper.appendChild(container);
 
-
-    for (let i = 0; i < newGrid; i++) {
-
-        const newContainer = document.createElement("div");
-        newContainer.classList.add("outer");
-        container.appendChild(newContainer);
-
-    for (let z = 0; z < newGrid; z++) {
-
-        const divs = document.createElement("div");
-        divs.textContent = "";
-        divs.classList.add("square");
-        divs.addEventListener("mouseenter", () => {
-        divs.style.backgroundColor = "blue";
-        });
-
-        newContainer.appendChild(divs);
-        }
-    }
+    createGrid(newGrid);
 });
 
 
